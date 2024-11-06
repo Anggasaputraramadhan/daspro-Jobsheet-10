2b6d5283555e4f69d184f627bff7c0317a47e1ca
Anggasaputraramadhan <anggasaputraramadhan5@gmail.com>
2024-11-06 11:04:28 +0700

    Add files via upload

--- BIOSKOP05.java
+++ BIOSKOP05.java
@@ -14,11 +14,49 @@ public class BIOSKOP05 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hadi";
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
+
+        System.out.println("\nJumlah baris dalam array penonton (penonton.length): " + penonton.length);
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang kolom pada baris ke-" + (i + 1) + " (penonton[" + i + "].length): " + penonton[i].length);
+        }
+
+        int barisKe = 1;
+        for (String[] baris : penonton) {
+            System.out.println("Panjang kolom pada baris ke-" + barisKe + " (baris.length): " + baris.length);
+            barisKe++;
+        }
+
+        System.out.println("\nNama penonton pada baris ke-3:");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+
+        System.out.println("\nNama penonton pada baris ke-3:");
+        for (String nama : penonton[2]) {
+            System.out.println(nama);
+        }
+
+        System.out.println("Daftar Penonton:");
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.print("Baris ke-" + (i + 1) + ": ");
+            for (String nama : penonton[i]) {
+                System.out.print(nama + " ");
+            }
+            System.out.println();
+        }
     
     }
 }
