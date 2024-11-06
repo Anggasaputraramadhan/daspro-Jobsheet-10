bf5593d19dc7d30c4d46e168a408b3d1aed72f5c
Anggasaputraramadhan <anggasaputraramadhan5@gmail.com>
2024-11-06 11:17:54 +0700

    Add files via upload

--- BioskopWithScanner05.java
+++ BioskopWithScanner05.java
@@ -4,28 +4,60 @@ public class BioskopWithScanner05 {
 
     public static void main(String[] args) {
         Scanner sc05 = new Scanner(System.in);
-        int baris, kolom;
-        String nama, next;
 
         String[][] penonton = new String[4][2];
+        int menuOption;
+        boolean isRunning = true;
 
-        while (true) {
-            System.out.print("Masukan nama: ");
-            nama = sc05.nextLine();
-            System.out.print("Masukan baris: ");
-            baris = sc05.nextInt();
-            System.out.print("Masukan kolom: ");
-            kolom = sc05.nextInt();
+        while (isRunning) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1/2/3): ");
+            menuOption = sc05.nextInt();
             sc05.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            switch (menuOption) {
+                case 1:
+                    inputPenonton(penonton, sc05);
+                    break;
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc05.nextLine();
+                case 2:
+                    tampilkanPenonton(penonton);
+                    break;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                case 3:
+                    System.out.println("Keluar dari program...");
+                    isRunning = false;
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid. Coba lagi.");
             }
+        }
+
+        sc05.close();
+    }
+
+    public static void inputPenonton(String[][] penonton, Scanner sc) {
+        for (int i = 0; i < penonton.length; i++) {
+            for (int j = 0; j < penonton[i].length; j++) {
+                System.out.print("Masukkan nama penonton pada baris " + (i + 1) + ", kolom " + (j + 1) + ": ");
+                penonton[i][j] = sc.nextLine();
+            }
+        }
+        System.out.println("Data penonton berhasil diinput.");
+    }
+
+    public static void tampilkanPenonton(String[][] penonton) {
+        System.out.println("\nDaftar Penonton:");
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.print("Baris ke-" + (i + 1) + ": ");
+            for (int j = 0; j < penonton[i].length; j++) {
+                System.out.print(penonton[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
